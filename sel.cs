2a9b977e720a976122af1b386c873fdc334d7177
Vixtor61 <00198117@uca.edu.sv>
2021-07-02 14:34:51 -0600

    asdf

--- sel.cs
+++ sel.cs
@@ -144,20 +144,21 @@ public Matrix createLocalK(int element,mesh m){
     // K = (k*Ve/D^2)Bt*At*A*B := K_4x4
     float D,Ve,EI = m.getEI();
     Matrix K = new Matrix();
-    Matrix A= new Matrix();
-    Matrix B= new Matrix();
     Matrix U= new Matrix();
-    Matrix At= new Matrix();
+
 
 
     
 
-    D = calculateLocalD(element,m);
-    Ve = 3;
+  
+    
 
     //math.zeroes(A,3);
     math.zeroes(U,10,10);
     calculateLocalU(U);
+    fillLocalK(K,U);
+
+
    // calculateLocalA(element,A,m);
    // calculateB(B);
   // math.transpose(A,At);
@@ -167,6 +168,22 @@ public Matrix createLocalK(int element,mesh m){
  
     return K;
 }
+
+public void fillLocalK(Matrix K,Matrix U){
+    math.zeroes(K,30,30);
+    int Usize = U.Count;
+    int Usizex2 = 2* U.Count;
+    for (int i = 0; i < Usize; i++)
+    {
+        for (int j = 0; j < USize; j++)
+        {
+            K[i][j] = U[i][j];
+            K[i + Usize][j+Usize] = U[i][j];
+            K[i + Usizex2][j+ Usizex2] = U[i][j];
+            
+        }
+    }
+} 
 
 public float calculateLocalJ(int ind,mesh m){
     float J,a,b,c,d,e,f,g,h,i;
@@ -214,24 +231,135 @@ public void assemblyK(element e,Matrix localK,Matrix K){
     int index2 = e.getNode2() - 1;
     int index3 = e.getNode3() - 1;
     int index4 = e.getNode4() - 1;
+    int index5 = e.getNode5() - 1;
+    int index6 = e.getNode6() - 1;
+    int index7 = e.getNode7() - 1;
+    int index8 = e.getNode8() - 1;
+    int index9 = e.getNode9() - 1;
+    int index10 = e.getNode10() - 1;
  
 
     K[index1][index1] += localK[0][0];
     K[index1][index2] += localK[0][1];
     K[index1][index3] += localK[0][2];
     K[index1][index4] += localK[0][3];
+	K[index1][index5] += localK[0][4];
+    K[index1][index6] += localK[0][5];
+    K[index1][index7] += localK[0][6];
+    K[index1][index8] += localK[0][7];
+	K[index1][index9] += localK[0][8];
+    K[index1][index10] += localK[0][9];
+
     K[index2][index1] += localK[1][0];
     K[index2][index2] += localK[1][1];
     K[index2][index3] += localK[1][2];
     K[index2][index4] += localK[1][3];
+	K[index2][index5] += localK[1][4];
+    K[index2][index6] += localK[1][5];
+    K[index2][index7] += localK[1][6];
+    K[index2][index8] += localK[1][7];
+	K[index2][index9] += localK[1][8];
+    K[index2][index10] += localK[1][9];
+
+
     K[index3][index1] += localK[2][0];
     K[index3][index2] += localK[2][1];
     K[index3][index3] += localK[2][2];
     K[index3][index4] += localK[2][3];
+	K[index3][index5] += localK[2][4];
+    K[index3][index6] += localK[2][5];
+    K[index3][index7] += localK[2][6];
+    K[index3][index8] += localK[2][7];
+	K[index3][index9] += localK[2][8];
+    K[index3][index10] += localK[2][9];
+
     K[index4][index1] += localK[3][0];
     K[index4][index2] += localK[3][1];
     K[index4][index3] += localK[3][2];
     K[index4][index4] += localK[3][3];
+	K[index4][index5] += localK[3][4];
+    K[index4][index6] += localK[3][5];
+    K[index4][index7] += localK[3][6];
+    K[index4][index8] += localK[3][7];
+	K[index4][index9] += localK[3][8];
+    K[index4][index10] += localK[3][9];
+
+
+    K[index5][index1] += localK[4][0];
+    K[index5][index2] += localK[4][1];
+    K[index5][index3] += localK[4][2];
+    K[index5][index4] += localK[4][3];
+	K[index5][index5] += localK[4][4];
+    K[index5][index6] += localK[4][5];
+    K[index5][index7] += localK[4][6];
+    K[index5][index8] += localK[4][7];
+	K[index5][index9] += localK[4][8];
+    K[index5][index10] += localK[4][9];
+
+
+    K[index6][index1] += localK[5][0];
+    K[index6][index2] += localK[5][1];
+    K[index6][index3] += localK[5][2];
+    K[index6][index4] += localK[5][3];
+	K[index6][index5] += localK[5][4];
+    K[index6][index6] += localK[5][5];
+    K[index6][index7] += localK[5][6];
+    K[index6][index8] += localK[5][7];
+	K[index6][index9] += localK[5][8];
+    K[index6][index10] += localK[5][9];
+
+
+    K[index7][index1] += localK[6][0];
+    K[index7][index2] += localK[6][1];
+    K[index7][index3] += localK[6][2];
+    K[index7][index4] += localK[6][3];
+	K[index7][index5] += localK[6][4];
+    K[index7][index6] += localK[6][5];
+    K[index7][index7] += localK[6][6];
+    K[index7][index8] += localK[6][7];
+	K[index7][index9] += localK[6][8];
+    K[index7][index10] += localK[6][9];
+
+    K[index8][index1] += localK[7][0];
+    K[index8][index2] += localK[7][1];
+    K[index8][index3] += localK[7][2];
+    K[index8][index4] += localK[7][3];
+	K[index8][index5] += localK[7][4];
+    K[index8][index6] += localK[7][5];
+    K[index8][index7] += localK[7][6];
+    K[index8][index8] += localK[7][7];
+	K[index8][index9] += localK[7][8];
+    K[index8][index10] += localK[7][9];
+
+    K[index9][index1] += localK[8][0];
+    K[index9][index2] += localK[8][1];
+    K[index9][index3] += localK[8][2];
+    K[index9][index4] += localK[8][3];
+	K[index9][index5] += localK[8][4];
+    K[index9][index6] += localK[8][5];
+    K[index9][index7] += localK[8][6];
+    K[index9][index8] += localK[8][7];
+	K[index9][index9] += localK[8][8];
+    K[index9][index10] += localK[8][9];
+
+    K[index10][index1] += localK[9][0];
+    K[index10][index2] += localK[9][1];
+    K[index10][index3] += localK[9][2];
+    K[index10][index4] += localK[9][3];
+	K[index10][index5] += localK[9][4];
+    K[index10][index6] += localK[9][5];
+    K[index10][index7] += localK[9][6];
+    K[index10][index8] += localK[9][7];
+	K[index10][index9] += localK[9][8];
+    K[index10][index10] += localK[9][9];
+
+
+
+
+
+
+
+;
 }
 
 public void assemblyb(element e,List<float> localb,List<float> b){
@@ -239,12 +367,25 @@ public void assemblyb(element e,List<float> localb,List<float> b){
     int index2 = e.getNode2() - 1;
     int index3 = e.getNode3() - 1;
     int index4 = e.getNode4() - 1;
+    int index5 = e.getNode5() - 1;
+    int index6 = e.getNode6() - 1;
+    int index7 = e.getNode7() - 1;
+    int index8 = e.getNode8() - 1;
+    int index9 = e.getNode9() - 1;
+    int index10 = e.getNode10() - 1;
 
     b[index1] += localb[0];
     b[index2] += localb[1];
     b[index3] += localb[2];
     b[index4] += localb[3];
+    b[index5] += localb[4];
+    b[index6] += localb[5];
+    b[index7] += localb[6];
+    b[index8] += localb[7];
+    b[index9] += localb[8];
+    b[index10] += localb[9];
     
+
 }
 
 public void ensamblaje(mesh m,List<Matrix> localKs,List<List<float>> localbs,Matrix K,List<float> b){
